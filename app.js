527133a123b97c9f2148c4adb81f4095242a7ee2
Gertkhk <gert.sarv@khk.ee>
2022-09-20 11:21:03 +0300

    Project files

---
+++ app.js
@@ -0,0 +1,27 @@
+// variables
+// let
+
+let name = "Gert Sarv"
+name = "New name"
+
+// const
+// noinspection SpellCheckingInspection
+const surname = 'Sarv'
+// surname = 'Sarv' - error
+
+// arrays
+const numbers = [1, 2, 3, 4, 5,]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+// objects
+const person = {
+    firstname: "Gert",
+    surname: "Sarv"
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = 'IKT'
+console.log(person)
